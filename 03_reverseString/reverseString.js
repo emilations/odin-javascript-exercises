dc52b8b244a01416da5aefa9238231305e78472e
Emile Mouannes <emile.mouannes@outlook.com>
2022-06-02 21:31:40 -0400

    Add solution to reverseString

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,10 @@
-const reverseString = function() {
-
+const reverseString = function(string) {
+    let stringSplit = string.split("");
+    let stringReversed = [];
+    for (let i = 0; i < stringSplit.length; i++) {
+        stringReversed[i] = stringSplit[stringSplit.length - i - 1];
+    }
+    return stringReversed.join("")
 };
 
 // Do not edit below this line
