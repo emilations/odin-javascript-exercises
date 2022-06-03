b7e386032f915004004cd873aaee1a95dc6fca05
Emile Mouannes <emile.mouannes@outlook.com>
2022-06-03 11:38:11 -0400

    Add solution sumAll()

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,12 @@
-const sumAll = function() {
+const sumAll = function(start, end) {
+    let count = 0;
+    if (!(Number.isInteger(start) && Number.isInteger(end))) return "ERROR"
 
+    let input = [...arguments].sort((a, b) => a - b);
+    if (input[0] < 0 || input[1] < 0) return "ERROR"
+
+    for (let i = input[0]; i <= input[1]; i++) count += i;
+    return count;
 };
 
 // Do not edit below this line
