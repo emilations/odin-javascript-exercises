660189d55a8a33767acf38403695155ca783a637
Emile Mouannes <emile.mouannes@outlook.com>
2022-06-02 21:00:26 -0400

    Add solution to repeatString

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,9 @@
-const repeatString = function() {
-
+const repeatString = function(word, times) {
+    let result = (times < 0) ? "ERROR" : "";
+    for (let i = 0; i < times; i++) {
+        result = result + word;
+    }
+    return result
 };
 
 // Do not edit below this line
