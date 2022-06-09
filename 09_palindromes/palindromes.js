0b2855aa1285271f170c8a3d744dba593b73c958
Emile Mouannes <emile.mouannes@outlook.com>
2022-06-09 14:32:15 -0400

    Add solution palindromes

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,10 @@
-const palindromes = function () {
-
+const palindromes = function (input) {
+    let regEx = /[a-zA-Z]/;
+    let newInput = input.split("")
+                        .filter((letter) => regEx.test(letter))
+                        .map((char) => char.toLowerCase())
+                        .join("")
+    return (newInput.split("").reverse().join("") == newInput);
 };
 
 // Do not edit below this line
