5a0cda7dd245c337dfedf5ae3e696826c4e7ecdc
Emile Mouannes <emile.mouannes@outlook.com>
2022-06-11 22:36:41 -0400

    Add solution to find the oldest

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,6 +1,15 @@
-const findTheOldest = function() {
-
-};
+const findTheOldest = function(people) {
+    return people.reduce(function(previousPerson, currentPerson) {
+        let yearOfDeath = (currentPerson.hasOwnProperty("yearOfDeath")) ? currentPerson.yearOfDeath : (new Date()).getFullYear();
+        console.log(yearOfDeath)
+        let ageCalculated = yearOfDeath - currentPerson.yearOfBirth;
+        if (ageCalculated > previousPerson.age) {
+            return {name: currentPerson.name, age: ageCalculated};
+        } else {
+            return previousPerson;
+        }
+    }, {age: 0})
+}
 
 // Do not edit below this line
 module.exports = findTheOldest;
